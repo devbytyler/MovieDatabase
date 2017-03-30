d3f62d0cd4822b017b9c81b84b821277366706d4
tylerstephens814 <tylerstephens814@gmail.com>
2017-03-30 12:00:40 -0600

    Setup has_many :through model associations

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,4 @@
 class Actor < ApplicationRecord
+  has_many :roles, dependent: :destroy
+  has_many :movies, -> { uniq }, through: :roles
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ApplicationRecord
+  has_many :roles, dependent: :destroy#, inverse_of: :actor, foreign_key: :actor_id
+  has_many :actors, -> { distinct }, through: :roles
+
+  validates_presence_of :title, :genre, :rating
+
+  accepts_nested_attributes_for :roles, :reject_if => :all_blank, :allow_destroy => true
+  accepts_nested_attributes_for :actors
 end
