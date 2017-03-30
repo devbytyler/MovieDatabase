8f05cb3e0af6899875078a6e267140589c9590c0
tylerstephens814 <tylerstephens814@gmail.com>
2017-03-30 12:17:08 -0600

    Implement actor methods

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,7 +1,18 @@
 class ActorsController < ApplicationController
+
+  before_action :set_actor, only: [:show]
+  
   def index
+    @actors = Actor.all
   end
 
   def show
   end
+
+  private
+
+  def set_actor
+    @actor = Actor.find(params[:id])
+  end
 end
+
